91dfa17a9b0d7b422140de49f02e9ef79732fc47
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-16 15:17:08 +0800

    feat: make public symbols get into a public header.

---
+++ src/config.hpp
@@ -0,0 +1,9 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+namespace bpt {
+using page_id_t = int;
+using frame_id_t = int;
+const page_id_t INVALID_PAGE_ID = -1;
+const frame_id_t INVALID_FRMAE_ID = -1;
+} // namespace bpt
+#endif
