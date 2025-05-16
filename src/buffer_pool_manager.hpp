853f47312acc3f4e170b562fa71c2f2569fe767d
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-16 14:57:59 +0800

    feat: finish building the bpm .

--- src/buffer_pool_manager.hpp
+++ src/buffer_pool_manager.hpp
@@ -6,16 +6,57 @@ namespace bpt {
 class PageGuard {
 private:
   char *pointer_;
+  FrameManager *frame_manager_;
   page_id_t page_in_;
+  frame_id_t frame_in_;
+  bool is_valid_;
 
 public:
-  PageGuard(char *pointer, page_id_t page_in){};
+  PageGuard() : is_valid_(false) {}
+  PageGuard(char *pointer, page_id_t page_in, frame_id_t frame_in,
+            FrameManager *frame_manager)
+      : pointer_(pointer), page_in_(page_in), frame_in_(frame_in),
+        frame_manager_(frame_manager), is_valid_(true) {}
   PageGuard(const PageGuard &other) = delete;
-  PageGuard(PageGuard &&other){};
-  auto operator=(PageGuard &&other) -> PageGuard &{};
-  auto GetPageID() -> page_id_t{};
-  template <typename T> auto As() -> const T *{};
-  template <typename T> auto AsMut() -> T *{};
+  PageGuard(PageGuard &&other) noexcept {
+    if (other.is_valid_) {
+      pointer_ = std::exchange(other.pointer_, nullptr);
+      frame_manager_ = std::exchange(other.frame_manager_, nullptr);
+      page_in_ = other.page_in_;
+      frame_in_ = other.frame_in_;
+      is_valid_ = true;
+      other.is_valid_ = false;
+    }
+  }
+  auto operator=(PageGuard &&other) -> PageGuard & {
+    if (this != &other) {
+      if (is_valid_ == true) {
+        frame_manager_->Unpin(frame_in_);
+      }
+      pointer_ = std::exchange(other.pointer_, nullptr);
+      frame_manager_ = std::exchange(other.frame_manager_, nullptr);
+      page_in_ = other.page_in_;
+      frame_in_ = other.frame_in_;
+      is_valid_ = other.is_valid_;
+      other.is_valid_ = false;
+    }
+  }
+
+  auto GetPageID() -> page_id_t { return page_in_; }
+
+  template <typename T> auto As() -> const T * {
+    return reinterpret_cast<const T *>(pointer_);
+  }
+
+  template <typename T> auto AsMut() -> T * {
+    return reinterpret_cast<T *>(pointer_);
+  }
+
+  ~PageGuard() {
+    if (is_valid_) {
+      frame_manager_->Unpin(frame_in_);
+    }
+  }
 };
 
 class BufferPoolManager {
@@ -24,19 +65,74 @@ private:
   std::unordered_map<page_id_t, frame_id_t> page_table_;
   DiskManager *disk_manager_;
   FrameManager *frame_manager_;
+  int cache_size_;
+  int page_size_;
   char **cache_;
 
-  auto FindFrame(page_id_t target_page) -> std::pair<frame_id_t, page_id_t>{};
+  void FetchPage(page_id_t target_page, frame_id_t target_frame) {
+    data_file_.seekg(target_page * page_size_);
+    data_file_.read(cache_[target_frame], page_size_);
+  }
+  void FlushPage(page_id_t target_page, frame_id_t target_frame) {
+    data_file_.seekp(target_page * page_size_);
+    data_file_.write(cache_[target_frame], page_size_);
+  }
 
 public:
   BufferPoolManager() = delete;
-  BufferPoolManager(int cache_size, int page_size){};
-  ~BufferPoolManager(){};
+  BufferPoolManager(int cache_size, int page_size, std::string &data_file,
+                    std::string &disk_manager_file) {
+    data_file_.open(data_file, std::ios::in | std::ios::out | std::ios::binary);
+    if (!data_file_.good()) {
+      data_file_.close();
+      data_file_.open(data_file, std::ios::out | std::ios::binary);
+    }
+    frame_manager_ = new FrameManager(cache_size);
+    disk_manager_ = new DiskManager(disk_manager_file);
+    cache_size_ = cache_size;
+    cache_ = new char *[cache_size];
+    for (int i = 0; i < cache_size; ++i) {
+      cache_[i] = new char[page_size];
+    }
+    page_size_ = page_size;
+  }
+  ~BufferPoolManager() {
+    for (auto iter = page_table_.begin(); iter != page_table_.end(); ++iter) {
+      FlushPage(iter->first, iter->second);
+    }
+    data_file_.close();
+    delete frame_manager_;
+    delete disk_manager_;
+    for (int i = 0; i < cache_size_; ++i) {
+      delete cache_[i];
+    }
+    delete cache_;
+  }
 
-  auto NewPage() -> page_id_t;
-  auto DeletePage(page_id_t target_page) -> bool;
+  auto NewPage() -> page_id_t { return disk_manager_->NewPage(); }
+  auto DeletePage(page_id_t target_page) -> bool {
+    disk_manager_->DeletePage(target_page);
+    if (page_table_.count(target_page)) {
+      frame_manager_->Erase(page_table_[target_page]);
+    }
+  }
 
-  auto ReadPage(page_id_t target_page) -> PageGuard{};
-  auto WritePage(page_id_t target_page) -> PageGuard{};
+  auto VisitPage(page_id_t target_page, bool read) -> PageGuard {
+    if (page_table_.count(target_page)) {
+      auto iter = page_table_.find(target_page);
+      frame_manager_->Pin(iter->second, read);
+      return PageGuard(cache_[iter->second], iter->first, iter->second,
+                       frame_manager_);
+    }
+    std::pair<frame_id_t, page_id_t> info = frame_manager_->EvictFrame();
+    if (info.second != INVALID_PAGE_ID && frame_manager_->IsDirty(info.first)) {
+      FlushPage(info.second, info.first);
+    }
+    FetchPage(target_page, info.first);
+    frame_manager_->ConnectPage(info.first, target_page);
+    frame_manager_->Pin(info.first, read);
+    return PageGuard(cache_[info.first], target_page, info.first,
+                     frame_manager_);
+  }
 };
 } // namespace bpt
--- src/frame_manager.hpp
+++ src/frame_manager.hpp
@@ -7,7 +7,7 @@ namespace bpt {
 using page_id_t = int;
 using frame_id_t = int;
 const page_id_t INVALID_PAGE_ID = -1;
-const frame_id_t INVAILD_FRMAE_ID = -1;
+const frame_id_t INVALID_FRMAE_ID = -1;
 
 struct FrameHeader {
   page_id_t page_in_;
@@ -17,9 +17,6 @@ struct FrameHeader {
 };
 
 class FrameManager {
-public:
-  enum class VisitType : bool { read = 0, write };
-
 private:
   std::vector<FrameHeader> frame_info_;
   int current_time_stamp_;
@@ -31,12 +28,15 @@ public:
   }
 
   auto EvictFrame() -> std::pair<frame_id_t, page_id_t> {
-    frame_id_t victim = INVAILD_FRMAE_ID;
+    frame_id_t victim = INVALID_FRMAE_ID;
     int time = current_time_stamp_;
     for (int i = 0; i < frame_info_.size(); ++i) {
       if (frame_info_[i].pin_count_ == 0 && frame_info_[i].history_ < time) {
         victim = i;
         time = frame_info_[i].history_;
+        if (frame_info_[i].page_in_ == INVALID_PAGE_ID) {
+          break;
+        }
       }
     }
     return {victim, frame_info_[victim].page_in_};
@@ -46,11 +46,11 @@ public:
     frame_info_[target_frame].page_in_ = target_page;
   }
 
-  void Pin(frame_id_t target_frame, VisitType is_read) {
+  void Pin(frame_id_t target_frame, bool is_read) {
     ++current_time_stamp_;
     frame_info_[target_frame].history_ = current_time_stamp_;
     ++frame_info_[target_frame].pin_count_;
-    if (is_read == VisitType::write) {
+    if (is_read == false) {
       frame_info_[target_frame].is_dirty_ = true;
     }
   };
@@ -60,12 +60,15 @@ public:
     --frame_info_[target_frame].pin_count_;
   };
 
+  void Erase(frame_id_t target_frame) {
+    frame_info_[target_frame].history_ = 0;
+    frame_info_[target_frame].is_dirty_ = false;
+    frame_info_[target_frame].pin_count_ = 0;
+    frame_info_[target_frame].page_in_ = 0;
+  }
+  
   auto IsDirty(frame_id_t target_frame) -> bool {
     return frame_info_[target_frame].is_dirty_;
-  }
-
-  auto PageIn(frame_id_t target_frame) -> page_id_t {
-    return frame_info_[target_frame].page_in_;
   }
 };
 } // namespace bpt
