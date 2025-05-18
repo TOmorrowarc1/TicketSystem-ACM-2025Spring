b59fb17fddff3832ee55be46d0bf010f87d4ed23
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-18 20:50:58 +0800

    feat: start building the b plus tree.

--- test/bpm_test.cpp
+++ test/bpm_test.cpp
@@ -6,26 +6,65 @@
 int main() {
   std::string file1 = "data_file";
   std::string file2 = "disk_file";
-  bpt::BufferPoolManager bpm(1, 4096, file1, file2);
+  const int FRAMES = 10;
+  bpt::BufferPoolManager bpm(FRAMES, 4096, file1, file2);
 
-  bpt::page_id_t pid = bpm.NewPage();
-  char str[] = "Hello, world!";
-  {
-    auto guard = bpm.VisitPage(pid, false);
-    char *data = guard.AsMut<char>();
-    snprintf(data, sizeof(str), "%s", str);
+  // Scenario: The buffer pool is empty. We should be able to create a new page.
+  bpt::page_id_t pid0 = bpm.NewPage();
+  auto page0 = bpm.VisitPage(pid0, false);
+
+  // Scenario: Once we have a page, we should be able to read and write content.
+  snprintf(page0.AsMut<char>(), 4096, "Hello");
+  assert(!std::strcmp(page0.AsMut<char>(), "Hello"));
+
+  page0.~PageGuard();
+
+  // Create a vector of unique pointers to page guards, which prevents the
+  // guards from getting destructed.
+  std::vector<bpt::PageGuard> pages;
+
+  // Scenario: We should be able to create new pages until we fill up the buffer
+  // pool.
+  for (size_t i = 0; i < FRAMES; i++) {
+    auto pid = bpm.NewPage();
+    auto page = bpm.VisitPage(pid, 0);
+    pages.push_back(std::move(page));
   }
-  // Check `ReadPageGuard` basic functionality.
-  {
-    auto guard = bpm.VisitPage(pid, true);
-    const char *data = guard.As<char>();
-    assert(strcmp(data, str));
+
+  // Scenario: All of the pin counts should be 1.
+
+  // Scenario: Once the buffer pool is full, we should not be able to create any
+  // new pages.
+
+  // Scenario: Drop the first 5 pages to unpin them.
+  for (size_t i = 0; i < FRAMES / 2; i++) {
+    pages.erase(pages.begin());
   }
-  // Check `ReadPageGuard` basic functionality (again).
-  {
-    auto guard = bpm.VisitPage(pid, true);
-    const char *data = guard.As<char>();
-    assert(strcmp(data, str));
+
+  // Scenario: All of the pin counts of the pages we haven't dropped yet should
+  // still be 1.
+
+  // Scenario: After unpinning pages {1, 2, 3, 4, 5}, we should be able to
+  // create 4 new pages and bring them into memory. Bringing those 4 pages into
+  // memory should evict the first 4 pages {1, 2, 3, 4} because of LRU.
+  for (size_t i = 0; i < ((FRAMES / 2) - 1); i++) {
+    auto pid = bpm.NewPage();
+    auto page = bpm.VisitPage(pid, false);
+    pages.push_back(std::move(page));
   }
-  bpm.DeletePage(pid);
+
+  // Scenario: There should be one frame available, and we should be able to
+  // fetch the data we wrote a while ago.
+  {
+    bpt::PageGuard original_page = bpm.VisitPage(pid0, true);
+    assert(!std::strcmp(original_page.As<char>(), "Hello"));
+  }
+
+  // Scenario: Once we unpin page 0 and then make a new page, all the buffer
+  // pages should now be pinned. Fetching page 0 again should fail.
+  auto last_pid = bpm.NewPage();
+  auto last_page = bpm.VisitPage(last_pid, false);
+
+  auto fail = bpm.VisitPage(pid0, true);
+  return 0;
 }
