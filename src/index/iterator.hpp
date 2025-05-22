a5f6c8e752c89c5cde8497138a45a4deba3ec27f
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-22 22:39:37 +0800

    fix: access through the guard.

--- src/index/iterator.hpp
+++ src/index/iterator.hpp
@@ -8,54 +8,49 @@ namespace bpt {
 
 TEMPLATE
 class IndexIterator {
-  using Leaf = LeafPage<KeyType, ValueType, KeyComparator>;
 
 private:
+  PageGuard page_guard_;
   BufferPoolManager *bpm_;
-  Leaf *page_;
+  Leaf *page_pointer_;
   int place_;
 
 public:
   IndexIterator() = delete;
-  IndexIterator(BufferPoolManager *bpm, Leaf *page, int place = 0)
-      : bpm_(bpm), page_(page), place_(place){};
+  IndexIterator(BufferPoolManager *bpm, PageGuard &&page, int place = 0)
+      : bpm_(bpm), page_guard_(std::move(page)), place_(place) {
+    page_pointer_ = page.AsMut<LEAF_PAGE_TYPE>();
+  };
   ~IndexIterator() = default;
 
-  auto IsEnd() -> bool { return page_ == nullptr && place_ == 0; };
+  auto IsEnd() -> bool { return (page_pointer_ == nullptr) && (place_ == 0); };
 
   auto operator*() -> std::pair<const KeyType &, const ValueType &> {
-    return {page_->KeyAt(place_), page_->ValueAt(place_)};
+    return {page_pointer_->KeyAt(place_), page_pointer_->ValueAt(place_)};
   }
 
   auto operator++() -> IndexIterator & {
-    if (page_ == nullptr) {
+    ++place_;
+    if (place_ == page_pointer_->GetSize()) {
       place_ = 0;
-    } else {
-      ++place_;
-      while (place_ >= page_->GetSize()) {
-        place_ = 0;
-        page_id_t next_page = page_->GetNextPageId();
-        if (next_page == INVALID_PAGE_ID) {
-          page_ = nullptr;
-          break;
-        }
-        auto guard = bpm_->VisitPage(next_page, false);
-        page_ = guard.template AsMut<Leaf>();
+      if (page_pointer_->GetNextPageId() == -1) {
+        page_pointer_ = nullptr;
+        return *this;
       }
+      page_guard_ = bpm_->ReadPage(page_pointer_->GetNextPageId());
+      page_pointer_ = page_guard_.As<LEAF_PAGE_TYPE>();
     }
     return *this;
   }
 
   auto operator==(const IndexIterator &itr) const -> bool {
-    return (page_ == itr.page_) && (place_ == itr.place_);
+    return (page_pointer_ == itr.page_pointer_) && (place_ == itr.place_);
   }
 
   auto operator!=(const IndexIterator &itr) const -> bool {
-    return (page_ != itr.page_) || (place_ != itr.place_);
+    return (page_pointer_ != itr.page_pointer_) || (place_ != itr.place_);
   }
 
-  // A helper function print out the statement of the iterator.
-  auto Getinfo() const -> std::pair<Leaf *, int> { return {page_, place_}; }
 };
 } // namespace bpt
 
