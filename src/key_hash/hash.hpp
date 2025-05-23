09c4c6080f3c10e5b39190fb76503133cb73a430
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-23 09:44:47 +0800

    feat: start to build the hash.

---
+++ src/key_hash/hash.hpp
@@ -0,0 +1,34 @@
+#ifndef HASH_HPP
+#define HASH_HPP
+#include <string>
+struct Key {
+  unsigned long long int string_hash_;
+  int value_;
+
+  Key() : string_hash_(0), value_(0){};
+  Key(const std::string &index, int value) : value_(value) {
+    string_hash_ = djb2_hash(index);
+  }
+  auto djb2_hash(const std::string &target) -> unsigned long long int;
+};
+
+auto Key::djb2_hash(const std::string &target) -> unsigned long long int {
+  unsigned long long hash = 5381;
+  int c;
+  for (int place = 0; target[place] != 0; ++place) {
+    hash = ((hash << 5) + hash) + static_cast<int>(target[place]);
+  }
+  return hash;
+}
+
+struct KeyComparator {
+  auto operator()(const Key &lhs, const Key &rhs) -> int {
+    int result = (lhs.string_hash_ == rhs.string_hash_)
+                     ? (lhs.value_ - rhs.value_)
+                     : (lhs.string_hash_ - rhs.string_hash_);
+    return 0;
+  }
+};
+
+
+#endif
--- test/bpm_test.cpp
+++ test/bpm_test.cpp
@@ -1,165 +1,39 @@
 #include "index/b_plus_tree.hpp"
+#include "key_hash/hash.hpp"
 #include <cstring>
 #include <iostream>
 #include <string>
-class MyString {
-private:
-  char content[65] = {0};
-
-public:
-  MyString() = default;
-  MyString(const MyString &);
-  MyString(MyString &&);
-  MyString(const std::string &);
-  ~MyString() = default;
-  MyString &operator=(const MyString &);
-  MyString &operator=(const std::string &);
-  std::string return_content();
-  char &operator[](int);
-  bool operator>(const MyString &) const;
-  bool operator==(const MyString &) const;
-  bool operator!=(const MyString &) const;
-  bool operator>=(const MyString &) const;
-  bool operator<(const MyString &) const;
-  bool operator<=(const MyString &) const;
-  friend std::ostream &operator<<(std::ostream &output, const MyString &object);
-  friend std::istream &operator>>(std::istream &output, MyString &object);
-};
-MyString::MyString(const std::string &target) {
-  std::strcpy(content, &target[0]);
-}
-MyString::MyString(MyString &&other) { std::strcpy(content, other.content); }
-MyString::MyString(const MyString &other) {
-  std::strcpy(content, other.content);
-}
-std::string MyString::return_content() { return std::string(content); }
-char &MyString::operator[](int place) { return content[place]; }
-bool MyString::operator>(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) > 0;
-}
-bool MyString::operator==(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) == 0;
-}
-bool MyString::operator!=(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) != 0;
-}
-bool MyString::operator>=(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) >= 0;
-}
-bool MyString::operator<(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) < 0;
-}
-bool MyString::operator<=(const MyString &B) const {
-  return std::strcmp((*this).content, B.content) <= 0;
-}
-MyString &MyString::operator=(const MyString &B) {
-  if (&B != this) {
-    std::strcpy(content, B.content);
-  }
-  return *this;
-}
-MyString &MyString::operator=(const std::string &B) {
-  std::strcpy(content, &B[0]);
-  return *this;
-}
-std::ostream &operator<<(std::ostream &output, const MyString &object) {
-  return output << object.content;
-}
-std::istream &operator>>(std::istream &input, MyString &object) {
-  std::string temp;
-  input >> temp;
-  object = temp;
-  return input;
-}
-
-struct Key {
-  MyString key;
-  int value;
-};
-
-struct KeyComparator {
-  auto operator()(const Key &lhs, const Key &rhs) -> int {
-    if (lhs.key > rhs.key) {
-      return 1;
-    } else if (rhs.key > lhs.key) {
-      return -1;
-    } else {
-      if (lhs.value > rhs.value) {
-        return 1;
-      } else if (rhs.value > lhs.value) {
-        return -1;
-      }
-    }
-    return 0;
-  }
-};
 
 int main() {
   int operation_num = 0;
+  int value = 0;
   std::cin >> operation_num;
   std::string operation;
+  std::string index;
   std::string insert = "insert";
   std::string del = "delete";
   std::string find = "find";
   bpt::BufferPoolManager bpm(50, 4096, "data_file", "disk_file");
   bpm.NewPage();
   bpt::BPlusTree<Key, int, KeyComparator> storage(0, &bpm);
-  Key index;
+  Key key;
   std::vector<int> test;
-  /*for (int i = 0; i < 999; ++i) {
-    index.key = "Amiya";
-    index.value = i;
-    storage.Insert(index, index.value);
-    assert(storage.GetValue(index, &test));
-    assert(!storage.Insert(index, index.value));
-  }
-  int count = 0;
-  Key min{index.key, (1 << 31)};
-  Key max{index.key, ~(1 << 31)};
-  auto iter = storage.KeyBegin(min);
-  while (!iter.IsEnd() && KeyComparator{}((*iter).first, max) <= 0 &&
-         KeyComparator{}((*iter).first, min) >= 0) {
-    ++count;
-    std::cout << (*iter).second << ' ';
-    ++iter;
-  }
-  if (count == 0) {
-    std::cout << "null";
-  }
-  std::cout << "Checkpoint 1" << '\n';
-  for (int i = 0; i < 1000; i = i + 3) {
-    index.value = i;
-    storage.Remove(index);
-  }
-  count = 0;
-  min = {index.key, (1 << 31)};
-  max = {index.key, ~(1 << 31)};
-  auto iter1 = storage.KeyBegin(min);
-  while (!iter1.IsEnd() && KeyComparator{}((*iter1).first, max) <= 0 &&
-         KeyComparator{}((*iter1).first, min) >= 0) {
-    ++count;
-    std::cout << (*iter1).second << ' ';
-    ++iter1;
-  }
-  if (count == 0) {
-    std::cout << "null";
-  }
-  std::cout << '\n';
-  std::cout << "Checkpoint 2" << '\n';*/
   for (int i = 0; i < operation_num; ++i) {
-    std::cin >> operation >> index.key;
+    std::cin >> operation >> index;
     if (operation == insert) {
-      std::cin >> index.value;
-      storage.Insert(index, index.value);
-      assert(storage.GetValue(index, &test));
+      std::cin >> value;
+      key = Key(index, value);
+      storage.Insert(key, value);
+      assert(storage.GetValue(key, &test));
     } else if (operation == del) {
-      std::cin >> index.value;
-      storage.Remove(index);
-      assert(!storage.GetValue(index, &test));
+      std::cin >> value;
+      key = Key(index, value);
+      storage.Remove(key);
+      assert(!storage.GetValue(key, &test));
     } else {
       int count = 0;
-      Key min{index.key, (1 << 31)};
-      Key max{index.key, ~(1 << 31)};
+      Key min(index, (1 << 31));
+      Key max(index, ~(1 << 31));
       auto iter = storage.KeyBegin(min);
       while (!iter.IsEnd() && KeyComparator{}((*iter).first, max) <= 0 &&
              KeyComparator{}((*iter).first, min) >= 0) {
