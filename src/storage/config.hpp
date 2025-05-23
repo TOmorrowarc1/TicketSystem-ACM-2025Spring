72d1083a6d1a25adf5721a2d2e504a573f934acd
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-23 15:06:42 +0800

    fix: correct the logic mistakes in insert.

--- src/storage/config.hpp
+++ src/storage/config.hpp
@@ -4,6 +4,6 @@ namespace bpt {
 using page_id_t = int;
 using frame_id_t = int;
 const page_id_t INVALID_PAGE_ID = -1;
-const frame_id_t INVALID_FRMAE_ID = -1;
+const frame_id_t INVALID_FRAME_ID = -1;
 } // namespace bpt
 #endif
