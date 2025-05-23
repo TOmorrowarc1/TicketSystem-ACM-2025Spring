7d3a470e57801e26aec3c79f50e4591afb6af297
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-23 14:53:37 +0800

    0

--- test/bpm_test.cpp
+++ test/bpm_test.cpp
@@ -16,53 +16,6 @@ int main() {
   bpm.NewPage();
   bpt::BPlusTree<Key, int, KeyComparator> storage(0, &bpm);
   Key key;
-  std::vector<int> test;
-  /*{
-    for (int i = 0; i < 1000; ++i) {
-      key = Key("Amiya", i);
-      if (i == 913) {
-        std::cout << "?";
-      }
-      storage.Insert(key, key.value_);
-      assert(storage.GetValue(key, &test));
-      assert(!storage.Insert(key, key.value_));
-    }
-    int count = 0;
-    Key min("Amiya", (1 << 31));
-    Key max("Amiya", ~(1 << 31));
-    auto iter = storage.KeyBegin(min);
-    while (!iter.IsEnd() && KeyComparator{}((*iter).first, max) <= 0 &&
-           KeyComparator{}((*iter).first, min) >= 0) {
-      ++count;
-      std::cout << (*iter).second << ' ';
-      ++iter;
-    }
-    if (count == 0) {
-      std::cout << "null";
-    }
-    std::cout << "checkpoint1" << '\n';
-  }
-  {
-    for (int i = 0; i < 1000; i = i + 2) {
-      key = Key("Amiya", i);
-      storage.Remove(key);
-      assert(!storage.GetValue(key, &test));
-    }
-    int count = 0;
-    Key min("Amiya", (1 << 31));
-    Key max("Amiya", ~(1 << 31));
-    auto iter = storage.KeyBegin(min);
-    while (!iter.IsEnd() && KeyComparator{}((*iter).first, max) <= 0 &&
-           KeyComparator{}((*iter).first, min) >= 0) {
-      ++count;
-      std::cout << (*iter).second << ' ';
-      ++iter;
-    }
-    if (count == 0) {
-      std::cout << "null";
-    }
-    std::cout << "checkpoint2" << '\n';
-  }*/
   for (int i = 0; i < operation_num; ++i) {
     std::cin >> operation >> index;
     if (operation == insert) {
