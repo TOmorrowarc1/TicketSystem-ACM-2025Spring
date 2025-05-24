7435764c895bf976a37c561cef5ccc38a01ec719
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-24 20:54:41 +0800

    feat: end the part of user.

--- main/ioput.cpp
+++ main/ioput.cpp
@@ -1,7 +1,21 @@
-#include "system/core_system.hpp"
-#include "system/user_system.hpp"
+#include "commandparser.hpp"
 #include <iostream>
 
-int main(){
-  
+int main() {
+  std::string input;
+  std::string timestamp;
+  TokenScanner command;
+  UserCommand user_parser;
+  std::cin >> input;
+  command = input;
+  user_parser = UserParse(command);
+  user_sys::AddAdmin(user_parser.c_uid_, user_parser.para_);
+  while (input != "exit") {
+    std::cin >> input;
+    command = input;
+    timestamp = command.NextToken();
+    std::cout << timestamp;
+    Execute(UserParse(command));
+  }
+  return 0;
 }
