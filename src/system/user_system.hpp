20f588a191ac8363395e877edb550592b04f51e2
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-05-24 15:31:26 +0800

    feat: handle out the declaration of users part.

--- src/system/user_system.hpp
+++ src/system/user_system.hpp
@@ -8,7 +8,16 @@ bpt::BufferPoolManager user_info_buffer(50, 4096, "user_info_data",
 bpt::BPlusTree<FixedString<20>, UserInfo, FixStringComparator<20>>
     user_info(0, &user_info_buffer);
 
-
+auto AddAdmin(const FixedString<20> &uid) -> bool;
+auto AddUser(const FixedString<20> &c_uid, const FixedString<20> &uid,
+             const UserInfo &uinfo) -> bool;
+auto Seek(const FixedString<20> &c_uid, const FixedString<20> &uid) -> UserInfo;
+auto Modify(const FixedString<20> &c_uid, const FixedString<20> &uid,
+            const FixedString<30> *password, const FixedString<30> *mail,
+            const FixedChineseString<5> *name,
+            int privilege = INVALID_PRIVILEGE) -> bool;
+auto Login(const FixedString<20> &uid, const FixedString<30> &password);
+auto Logout(const FixedString<20> &uid);
 } // namespace user_sys
 
 #endif
