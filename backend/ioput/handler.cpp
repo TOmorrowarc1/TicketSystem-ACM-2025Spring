916d41647d943189a0b5a5afad33b381f1654063
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-06-05 17:19:40 +0800

    fix: the else should only be used in initalised situation.

--- backend/ioput/handler.cpp
+++ backend/ioput/handler.cpp
@@ -79,26 +79,26 @@ std::string ProcessCommand(const std::string &input1) {
   } else {
     has_opened.seekg(0);
     has_opened.read((char *)&train_sys::order_time, sizeof(int));
-  }
-  std::cin >> timestamp;
-  std::getline(std::cin, input);
-  command = input;
-  std::cout << timestamp << ' ';
-  if (input == " exit ") {
-    std::cout << "bye\n";
-  } else {
-    input = command.NextToken();
-    command.MoveBack();
-    if (input == "add_user" || input == "login" || input == "logout" ||
-        input == "query_profile" || input == "modify_profile") {
-      Execute(UserParse(command));
-      std::cout << '\n';
+    std::cin >> timestamp;
+    std::getline(std::cin, input);
+    command = input;
+    std::cout << timestamp << ' ';
+    if (input == " exit ") {
+      std::cout << "bye\n";
     } else {
-      Execute(command);
+      input = command.NextToken();
+      command.MoveBack();
+      if (input == "add_user" || input == "login" || input == "logout" ||
+          input == "query_profile" || input == "modify_profile") {
+        Execute(UserParse(command));
+        std::cout << '\n';
+      } else {
+        Execute(command);
+      }
     }
+    has_opened.seekp(0);
+    has_opened.write((char *)&train_sys::order_time, sizeof(int));
   }
-  has_opened.seekp(0);
-  has_opened.write((char *)&train_sys::order_time, sizeof(int));
 
   // 恢复IO
   std::cin.rdbuf(old_cin);
